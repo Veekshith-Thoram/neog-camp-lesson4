ac1aa9ea8fb5e3fe3d7c732f7c1270c214be6954
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-04-28 00:04:51 +0530

    taking input console output

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var btnTranslate = document.querySelector("#btn-tanslate");
+var txtInput = document.querySelector("#txt-input");
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
+console.log(txtInput);
+
+//this is what it means call back function!!
+function clickHandler() {
     console.log("Clicked!!");
-})
+    console.log("input", txtInput.value);
+}
+btnTranslate.addEventListener("click", clickHandler);
