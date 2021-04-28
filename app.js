fea767c4f573e2ec8a9489748d47d6dfebb41c71
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-04-29 02:45:35 +0530

    the minion is done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var btnTranslate = document.querySelector("#btn-tanslate");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputText = document.querySelector("#output")
 
