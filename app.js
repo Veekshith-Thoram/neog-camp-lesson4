05be21aaf5cf3390f1bab9c4c1a0c2a520737af7
Veekshith-Thoram <veekshiththoram@gmail.com>
2021-04-28 20:18:15 +0530

    input output processing code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var btnTranslate = document.querySelector("#btn-tanslate");
 var txtInput = document.querySelector("#txt-input");
+var outputText = document.querySelector("#output")
 
-console.log(txtInput);
 
 //this is what it means call back function!!
 function clickHandler() {
-    console.log("Clicked!!");
-    console.log("input", txtInput.value);
+    outputText.innerText = "Pushpa" + txtInput.value;
+    
 }
 btnTranslate.addEventListener("click", clickHandler);
